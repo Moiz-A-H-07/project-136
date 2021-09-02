fe3a05cbee30f6c428089de251776fd44163a03e
Moiz-A-H-07 <70146888+Moiz-A-H-07@users.noreply.github.com>
2021-09-02 16:18:25 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,24 @@
+from flask import Flask,jsonify,request
+from data import data
+
+app=Flask(__name__)
+@app.route("/")
+
+def index():
+    return jsonify({
+        "data":data,"message":"success"
+    }),200
+
+
+@app.route("/star")
+
+def star():
+    name=request.args.get("name")
+    stardata=next(item for  item in data if item["name"]==name)
+    return jsonify({
+        "data":stardata,"message":"success"
+    }),200
+
+
+if __name__=="__main__":
+    app.run()
